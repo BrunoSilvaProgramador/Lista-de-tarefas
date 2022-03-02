f1924d7575d7180f055c0cf9cbd3bc48d3eb6eaa
Bruno Silva Programador <brunosilvaprogramador@gmail.com>
2022-03-02 16:17:41 -0300

    Lista de tarefas concluída

--- ldt.js
+++ ldt.js
@@ -1,107 +1,47 @@
 var back = document.querySelector('.back');
-var result = document.querySelector('.result')
 var array = [];
-var quantidade = [];
-var quantidadeCalc = [];
-var precos = [];
-var precosCalc = [];
-var sit = [];
+
 
 load()
 function load(){
     let cont = document.querySelectorAll('.button_c');
     let des = document.querySelectorAll('.descricao-produto');
-    let apag = document.querySelectorAll('.apagar-produto');
-    let checkbox = document.querySelectorAll('#check');
-    let preco = document.querySelectorAll('#preço');
-    let quant = document.querySelectorAll('#quantitativo');
-    let menos = document.querySelectorAll('#menos');
-    let mais = document.querySelectorAll('#mais');
+    let apag = document.querySelectorAll('.excluir');
+    let check = document.querySelectorAll('.check');
+    let editar = document.querySelectorAll('.editar');
+    let p = document.querySelector('.p');
 
     for(let i = 0; i < cont.length; i++) {
         des[i].innerText = array[i];
         apag[i].addEventListener('click', function() {
             back.removeChild(cont[i]);
             array.splice(i, 1);
-            quantidade.splice(i, 1);
-            precos.splice(i, 1);
-            quantidadeCalc.splice(i, 1);
-            precosCalc.splice(i, 1);
-            result.innerText = 'R$ 0';
+            let container = document.querySelectorAll('.button_c');
+            if(container.length == 0) {
+                p.style.display = 'block';
+            }
         })
-        if(checkbox[i].checked){sit[i] = 1;} else{sit[i] = 0;}
-
-        if(quantidade[i] == 0){quant[i].value = '';} else{quant[i].value = quantidade[i];}
-
-        if(precos[i] == 0){preco[i].value = '';} else{preco[i].value = precos[i];}
-        
-        menos[i].addEventListener('click', function() { 
-            if(quant[i].value <=0){quant[i].value = 0;} else{quant[i].value--;}
+        check[i].addEventListener('click', function() {
+            check[i].classList.toggle('check_active');
         })
-
-        mais[i].addEventListener('click', function(){quant[i].value ++;})
+        editar[i].addEventListener('click', function() {
+            array[i] = prompt('Digite o nome da tarefa');
+            des[i].innerText = array[i];
+        })
     }
-    result.innerText = 'R$ 0';
+    if(cont.length > 0) {
+        p.style.display = 'none';
+    }
 }
 function add() {
-    calc()
-    var nome = prompt('Digite o nome do produto:');
-    if(nome.length <= 0){
+    var nome = document.getElementById('Nome');
+    if(nome.value.length <= 0){
         alert('Digite o nome do produto')
     }else{
-        array.push(nome);
-        back.innerHTML += '<div class="button_c"><span class="container-produtos"><div class="desc"><div class="descricao-produto"></div><input type="checkbox" id="check" checked></div><div class="dados"><div class="quantidade"><button class="quant" id="menos">-</button><input type="number" id="quantitativo" placeholder="Quant"><button class="quant" id="mais">+</button></div><input type="number" id="preço" placeholder="Preço"><button class="apagar-produto">x</button></div></span></div>';
+        array.push(nome.value);
+        back.innerHTML += '<div class="button_c"><span class="container-produtos"><div class="desc"><div class="descricao-produto"></div><div class="excluir"></div><div class="editar"></div><div class="check "></div></div></span></div>';
+        nome.value = '';
+        nome.focus();
         load()
     }
-    
-}
-
-function calc(){
-    let checkbox = document.querySelectorAll('#check');
-    let preco = document.querySelectorAll('#preço');
-    let quant = document.querySelectorAll('#quantitativo') 
-    for(let i=0; i < checkbox.length; i++){
-        if(sit[i] == 1){
-            if(checkbox[i].checked){                
-                if(quant[i].value == ''){quant[i].value = 0;}
-                if(preco[i].value == ''){preco[i].value = 0;}
-                sit[i] = 1;
-                quantidade[i] = quant[i].value;
-                precos[i] = preco[i].value;
-                quantidadeCalc[i] = quant[i].value;
-                precosCalc[i] = preco[i].value;
-            }else{
-                if(quant[i].value == 0){quant[i].value = '';}
-                if(preco[i].value == 0){preco[i].value = '';}
-                quantidadeCalc[i] = 0;
-                precosCalc[i] = 0;
-            }
-        }else{
-            if(checkbox[i].checked){ 
-                if(quant[i].value == ''){quant[i].value = 0;}
-                if(preco[i].value == ''){preco[i].value = 0;}
-                sit[i] = 1;
-                quantidade[i] = quant[i].value;
-                precos[i] = preco[i].value;
-                quantidadeCalc[i] = quant[i].value;
-                precosCalc[i] = preco[i].value;
-            }else{
-                if(quant[i].value == 0){quant[i].value = '';}
-                if(preco[i].value == 0){preco[i].value = '';}
-                quantidadeCalc[i] = 0;
-                precosCalc[i] = 0;
-            }
-        }  
-    }
-}
-
-
-function calcular(){
-    calc()
-    let cont = document.querySelectorAll('.container-produtos');
-    let resultado = 0;
-    for(let i = 0; i < cont.length; i++) {
-        resultado += (Number(quantidadeCalc[i] * precosCalc[i]));
-    }
-    result.innerText = `R$ ${resultado}`;
 }
